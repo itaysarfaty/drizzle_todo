2b6c148b14249d2e7b898618a3f5b8a1b70795d9
Itay Sarfaty <itaysarfaty@gmail.com>
2024-03-27 21:54:10 -0400

    fix: add key to complete button to fix bug that causes id in server action to not match id form prop

--- src/components/item.tsx
+++ src/components/item.tsx
@@ -5,7 +5,7 @@ export const Item = ({ description, id }: Todo) => {
     return (
         <li className="relative  flex items-center justify-between overflow-hidden rounded-md bg-gray-50">
             <p className="truncate px-3 py-3 text-sm ">{description}</p>
-            <Complete id={id} />
+            <Complete id={id} key={id} />
         </li>
     )
 }
